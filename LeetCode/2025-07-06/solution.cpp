c2a54d2b8b0ce0a14d00f01784fec295d15a3260
Prince <princemakwana1712@gmail.com>
2025-07-06 08:25:36 +0530

    Update solution.cpp

---
+++ LeetCode/2025-07-06/solution.cpp
@@ -0,0 +1,36 @@
+class FindSumPairs {
+public:
+    vector<int> nums1, nums2;
+    unordered_map<int, int> freq2;
+
+    FindSumPairs(vector<int>& n1, vector<int>& n2) {
+        nums1 = n1;
+        nums2 = n2;
+        for (int num : nums2)
+            freq2[num]++;
+    }
+    
+    void add(int index, int val) {
+        int oldVal = nums2[index];
+        freq2[oldVal]--;
+        nums2[index] += val;
+        freq2[nums2[index]]++;
+    }
+    
+    int count(int tot) {
+        int res = 0;
+        for (int a : nums1) {
+            int target = tot - a;
+            if (freq2.count(target))
+                res += freq2[target];
+        }
+        return res;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
